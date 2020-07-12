d15c016d42027bfe516602dfe5985a0c9efdc8d0
James Royston <jamesroyston03@gmail.com>
2020-07-12 02:22:19 -0500

    Add styles for searchbar, Add search icon svg to assets

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { authContext } from '../contexts/AuthContext';
 import { logout } from '../api/api';
+import { ReactComponent as Search } from '../assets/search.svg';
 
 const Container = styled.div`
   display: flex;
@@ -22,11 +23,34 @@ const Heading = styled.h1`
 `;
 
 const SearchBar = styled.div`
+  input {
+    line-height: 1;
+    margin-left: 11px;
+    font-size: 14px;
+    background: none;
+    border: none;
+    outline: none;
+    width: 100%;
+  }
   height: 2rem;
   width: 513px;
-  border: 1px solid #222222;
   border-radius: 4px;
+  background-color: #ebefef;
   margin-left: auto;
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: auto;
+    margin-right: 0;
+    border-radius: 0 4px 4px 0;
+    background-color: #222;
+    width: 37px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
 `;
 
 export default function Header() {
@@ -46,18 +70,29 @@ export default function Header() {
       </span>
     </>
   ) : (
-    <span>
-      <Link to="/classes" onClick={handleLogout}>
-        Logout
-      </Link>
-    </span>
+    <>
+      <span>
+        Good to see you, <strong>{store.get.user.substring(0, store.get.user.indexOf('@'))}</strong>
+        .
+      </span>
+      <span>
+        <Link to="/classes" onClick={handleLogout}>
+          Logout
+        </Link>
+      </span>
+    </>
   );
 
   return (
     <Container>
       <Heading>Classes</Heading>
       {store.get.loading ? '' : nav}
-      <SearchBar />
+      <SearchBar>
+        <input type="text" placeholder="Search" />
+        <span>
+          <Search />
+        </span>
+      </SearchBar>
     </Container>
   );
 }
