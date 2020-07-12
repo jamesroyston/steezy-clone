9af948fa5027fe54394f05910b395073ba978311
James Royston <jamesroyston03@gmail.com>
2020-07-12 15:20:05 -0500

    Add build for heroku deploy

---
+++ client/build/precache-manifest.3d4e6954cd8dc06afd552725d0592043.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9af9fbc38bfe5a959a9f311483b3a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd52e56d54b4c3faaedd",
+    "url": "/static/css/main.073122ef.chunk.css"
+  },
+  {
+    "revision": "4bd6989d9b65b12fb814",
+    "url": "/static/js/2.0bfe8df6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0bfe8df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd52e56d54b4c3faaedd",
+    "url": "/static/js/main.8ea1b594.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "46513bbc162e28a7b58eff745b33e594",
+    "url": "/static/media/class-thumbnail-1.46513bbc.jpg"
+  },
+  {
+    "revision": "5ce6186b9bd5911dee7480646f3d53e7",
+    "url": "/static/media/class-thumbnail-2.5ce6186b.jpg"
+  },
+  {
+    "revision": "8dd3d271b64f05ded868d885250cb305",
+    "url": "/static/media/class-thumbnail-3.8dd3d271.jpg"
+  },
+  {
+    "revision": "3f3b2aae0cc07ac23f8db54f8bc5a7f5",
+    "url": "/static/media/logo.3f3b2aae.svg"
+  },
+  {
+    "revision": "730f83b4db6ef84320ed99bbb670ae19",
+    "url": "/static/media/search.730f83b4.svg"
+  },
+  {
+    "revision": "bdfd53cda8640b53397e58252f910266",
+    "url": "/static/media/steezy.bdfd53cd.svg"
+  },
+  {
+    "revision": "ed52f187b2c4aad7fd02765c9cdc2f92",
+    "url": "/static/media/user.ed52f187.svg"
+  }
+]);
