5960c469c748a525c8cf8c408394f71341eb7da4
James Royston <jamesroyston03@gmail.com>
2020-07-15 10:24:04 -0500

    remove update to server.js for deploy (final push to github)

--- server.js
+++ server.js
@@ -56,11 +56,11 @@ app.use(userRouter);
 */
 
 // **serve static assets from client
-app.use(express.static(path.join(__dirname, 'client/build')))
+// app.use(express.static(path.join(__dirname, 'client/build')))
 
 // **serve index.html from client build folder
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
